1dd89ee88663812b87ccefe020c6310ed9ce4d04
raquelarauujo <r4quel.araujo@gmail.com>
2024-09-04 16:47:38 -0300

    Projeto atualizado

--- projeto-0/src/App.jsx
+++ projeto-0/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import FirstComponent from './Components/FirstComponent'
 import SecondComponent from './Components/SecondComponent'
-import ThirdComponent from './Components/ThirdComponent'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,9 +11,7 @@ function App() {
     <>
      <FirstComponent/>
      <SecondComponent/>
-     <ThirdComponent/>
-     
-     
+      
     </>
   )
 }
